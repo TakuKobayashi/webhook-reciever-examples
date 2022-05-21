5523ef970ba65c829976c0f1995bf435f200e327
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-22 01:30:45 +0900

    認証のドキュメントを見ながらOAuthを通してAccessTokenを取得するところまではできた

--- serverless/src/routes/platforms/withings.ts
+++ serverless/src/routes/platforms/withings.ts
@@ -1,31 +1,73 @@
 import { NextFunction, Request, Response } from 'express';
-import { stringifyUrl } from 'query-string';
+import axios from 'axios';
+import { stringify, stringifyUrl } from 'query-string';
+import { createHmac } from 'crypto';
 const { getCurrentInvoke } = require('@vendia/serverless-express');
 
 const express = require('express');
 const withingsRouter = express.Router();
 
-withingsRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+withingsRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
   res.send('hello withings');
 });
 
 withingsRouter.get('/login', (req: Request, res: Response, next: NextFunction) => {
-  const currentInvoke = getCurrentInvoke();
-  const currentBaseUrl = [req.protocol + '://' + req.get('host'), currentInvoke.event.requestContext.stage].join('/');
   // See this: https://developer.withings.com/api-reference#operation/oauth2-authorize
   const authorizeQueryObj = {
     response_type: 'code',
     client_id: process.env.WITHINGS_API_CLIENT_ID,
     state: 'foobar',
     scope: ['user.activity', 'user.metrics'].join(','),
-    redirect_uri: currentBaseUrl + '/platforms/withings/callback',
+    redirect_uri: getCallbackUrl(req),
   };
   console.log(authorizeQueryObj);
   res.redirect(stringifyUrl({ url: 'https://account.withings.com/oauth2_user/authorize2', query: authorizeQueryObj }));
 });
 
-withingsRouter.get('/callback', (req: Request, res: Response, next: NextFunction) => {
-  res.send('hello login');
+withingsRouter.get('/callback', async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.query.code) {
+    res.send('callback error');
+    return;
+  }
+  const nonce = await requestNonse();
+  // See this: https://developer.withings.com/developer-guide/v3/get-access/sign-your-requests/
+  const signature = createHmac('sha256', process.env.WITHINGS_API_SECRET)
+    .update(['requesttoken', process.env.WITHINGS_API_CLIENT_ID, nonce].join(','), 'utf8')
+    .digest('hex');
+  const oauthCallbackCode: string = req.query.code.toString();
+  const oauthObj = {
+    action: 'requesttoken',
+    client_id: process.env.WITHINGS_API_CLIENT_ID,
+    nonce: nonce,
+    signature: signature,
+    client_secret: process.env.WITHINGS_API_SECRET,
+    grant_type: 'authorization_code',
+    code: oauthCallbackCode,
+    redirect_uri: getCallbackUrl(req),
+  };
+  const oauthRes = await axios.post('https://wbsapi.withings.net/v2/oauth2', stringify(oauthObj));
+  res.json(oauthRes.data);
 });
+
+async function requestNonse(): Promise<string> {
+  // see this: https://developer.withings.com/api-reference/#tag/signature
+  const signaturetimestamp = Math.floor(new Date().getTime() / 1000);
+  const signatureObj = {
+    action: 'getnonce',
+    client_id: process.env.WITHINGS_API_CLIENT_ID,
+    timestamp: signaturetimestamp,
+    signature: createHmac('sha256', process.env.WITHINGS_API_SECRET)
+      .update(['getnonce', process.env.WITHINGS_API_CLIENT_ID, signaturetimestamp].join(','), 'utf8')
+      .digest('hex'),
+  };
+  const nonceRes = await axios.post('https://wbsapi.withings.net/v2/signature', stringify(signatureObj));
+  return nonceRes.data.body.nonce.toString();
+}
+
+function getCallbackUrl(req: Request): string {
+  const currentInvoke = getCurrentInvoke();
+  const currentBaseUrl = [req.protocol + '://' + req.get('host'), currentInvoke.event.requestContext.stage].join('/');
+  return currentBaseUrl + '/platforms/withings/callback';
+}
 
 export { withingsRouter };
