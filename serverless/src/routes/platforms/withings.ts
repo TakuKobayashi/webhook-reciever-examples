21eb52e8bb3527eb69fdd6030b869f13eb9e9cb9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-22 02:38:18 +0900

    共通でつい買いそうな部分を諸々分離してメソッド化させた

--- serverless/src/routes/platforms/withings.ts
+++ serverless/src/routes/platforms/withings.ts
@@ -28,29 +28,60 @@ withingsRouter.get('/callback', async (req: Request, res: Response, next: NextFu
     res.send('callback error');
     return;
   }
-  const oauthRes = await requestAccessToken(req);
+  const oauthRes = await requestGetAccessToken(req);
   // このような形で返ってくる {"status":0,"body":{"userid":"...","access_token":"...","refresh_token":"...","scope":"user.activity,user.metrics","expires_in":10800,"token_type":"Bearer"}}
   res.json(oauthRes.data);
 });
 
-async function requestAccessToken(req: Request): Promise<AxiosResponse<any, any>> {
-  const nonce = await requestNonse();
-  // See this: https://developer.withings.com/developer-guide/v3/get-access/sign-your-requests/
-  const signature = createHmac('sha256', process.env.WITHINGS_API_SECRET)
-    .update(['requesttoken', process.env.WITHINGS_API_CLIENT_ID, nonce].join(','), 'utf8')
-    .digest('hex');
+withingsRouter.post('/webhook', async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  console.log(req.query);
+  res.status(200);
+});
+
+interface RequestTokenSignatureBasic {
+  action: string;
+  client_id: string;
+  nonce: string;
+  signature: string;
+}
+
+async function requestGetAccessToken(req: Request): Promise<AxiosResponse<any, any>> {
+  const basicSignature: RequestTokenSignatureBasic = await constructNonceSignature('requesttoken');
   const oauthCallbackCode: string = req.query.code.toString();
-  const oauthObj = {
-    action: 'requesttoken',
-    client_id: process.env.WITHINGS_API_CLIENT_ID,
-    nonce: nonce,
-    signature: signature,
-    client_secret: process.env.WITHINGS_API_SECRET,
+  const requestTokenObj = {
     grant_type: 'authorization_code',
     code: oauthCallbackCode,
     redirect_uri: getCallbackUrl(req),
+    client_secret: process.env.WITHINGS_API_SECRET,
+    ...basicSignature,
   };
-  return axios.post('https://wbsapi.withings.net/v2/oauth2', stringify(oauthObj));
+  return axios.post('https://wbsapi.withings.net/v2/oauth2', stringify(requestTokenObj));
+}
+
+async function requestRefreshAccessToken(refresh_token: string): Promise<AxiosResponse<any, any>> {
+  const basicSignature: RequestTokenSignatureBasic = await constructNonceSignature('requesttoken');
+  const requestTokenObj = {
+    grant_type: 'refresh_token',
+    refresh_token: refresh_token,
+    client_secret: process.env.WITHINGS_API_SECRET,
+    ...basicSignature,
+  };
+  return axios.post('https://wbsapi.withings.net/v2/oauth2', stringify(requestTokenObj));
+}
+
+async function constructNonceSignature(action: string): Promise<RequestTokenSignatureBasic> {
+  const nonce = await requestNonse();
+  // See this: https://developer.withings.com/developer-guide/v3/get-access/sign-your-requests/
+  const signature = createHmac('sha256', process.env.WITHINGS_API_SECRET)
+    .update([action, process.env.WITHINGS_API_CLIENT_ID, nonce].join(','), 'utf8')
+    .digest('hex');
+  return {
+    action: action,
+    client_id: process.env.WITHINGS_API_CLIENT_ID,
+    nonce: nonce,
+    signature: signature,
+  };
 }
 
 async function requestNonse(): Promise<string> {
@@ -72,6 +103,12 @@ function getCallbackUrl(req: Request): string {
   const currentInvoke = getCurrentInvoke();
   const currentBaseUrl = [req.protocol + '://' + req.get('host'), currentInvoke.event.requestContext.stage].join('/');
   return currentBaseUrl + '/platforms/withings/callback';
+}
+
+function getWebhookUrl(req: Request): string {
+  const currentInvoke = getCurrentInvoke();
+  const currentBaseUrl = [req.protocol + '://' + req.get('host'), currentInvoke.event.requestContext.stage].join('/');
+  return currentBaseUrl + '/platforms/withings/webhook';
 }
 
 export { withingsRouter };
