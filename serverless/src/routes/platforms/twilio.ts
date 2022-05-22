2b5ec7f83ddb844f31a5c42ab96ea9f6a31c597d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-22 16:01:05 +0900

    音声とメッセージとをエンドポイントを分けた感じにした

--- serverless/src/routes/platforms/twilio.ts
+++ serverless/src/routes/platforms/twilio.ts
@@ -8,7 +8,23 @@ twilioRouter.get('/', async (req: Request, res: Response, next: NextFunction) =>
   res.json({ message: 'hello twilio' });
 });
 
-twilioRouter.post('/webhook', async (req: Request, res: Response, next: NextFunction) => {
+twilioRouter.post('/send_handler', async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  res.send('ok');
+});
+
+twilioRouter.post('/voice_webhook', async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  const payload = parse(req.body);
+  console.log(payload);
+  res.send(`
+    <Response>
+      <Say language="ja-jp">起きろー！！</Say>
+    </Response>
+  `);
+});
+
+twilioRouter.post('/message_webhook', async (req: Request, res: Response, next: NextFunction) => {
   console.log(req.body);
   const payload = parse(req.body);
   console.log(payload);
